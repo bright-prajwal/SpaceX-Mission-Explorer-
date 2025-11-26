8bbd9af1dcdcea4d3f03bc7a10881218c9586d98
bright-prajwal <honprajwal9@gmail.com>
2025-11-26 11:37:34 +0530

    Responsive/mobile layout

--- frontend/src/components/GalleryGrid.jsx
+++ frontend/src/components/GalleryGrid.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import ApodCard from "./ApodCard";
-
-// export default function GalleryGrid({ items = [] }) {
-//   return (
-//     <div className="grid">
-//       {items.map((item) => (
-//         <Link to={`/detail/${item.date}`} key={item.date} className="grid__item">
-//           <ApodCard apod={item} compact />
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import ApodCard from "./ApodCard";
-
-// export default function GalleryGrid({ items = [] }) {
-//   return (
-//     <div className="gallery-grid">
-//       {items.map((item) => (
-//         <Link
-//           to={`/detail/${item.date}`}
-//           key={item.date}
-//           className="gallery-grid-item"
-//         >
-//           <ApodCard apod={item} compact />
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// }
 
 
 import React from "react";
